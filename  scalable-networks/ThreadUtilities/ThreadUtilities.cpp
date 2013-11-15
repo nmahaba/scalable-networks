5eed26dc0cbf0e0198279dc70665114f785ff050
niranjan.quasar <niranjan.quasar@gmail.com>
2013-11-15 07:05:52 +0000

    Another UDP thread created for handling node entry messages like JoinReq and JoinResp messages. No intelligence is present in the functions yet. Need to come up with message structures also.

---  scalable-networks/ThreadUtilities/ThreadUtilities.cpp
+++  scalable-networks/ThreadUtilities/ThreadUtilities.cpp
@@ -10,6 +10,8 @@
 /* Extern */
 extern pthread_attr_t udpQueryThreadAttr;
 extern pthread_t udpQueryThread;
+extern pthread_attr_t udpEntryHandlerThreadAttr;
+extern pthread_t udpEntryHandlerThread;
 
 /*********************************************************************************************************
  /** spawnUdpThreadForQueries: Function to spawn a thread to handle UDP messages for node queries
@@ -27,7 +29,7 @@ int spawnUdpThreadForQueries()
 
 	pthread_attr_setdetachstate(&udpQueryThreadAttr, PTHREAD_CREATE_JOINABLE);
 
-	if((status = pthread_create(&udpQueryThread, &udpQueryThreadAttr, handleFileTransferRequest, 0)) == -1)
+	if((status = pthread_create(&udpQueryThread, &udpQueryThreadAttr, handleFileNodeQueries, 0)) == -1)
 	{
 		printf("ERROR: spawnUdpThreadForQueries, failed spawning a thread, ECODE %s\n", strerror(errno));
 		return -1;
@@ -37,11 +39,46 @@ int spawnUdpThreadForQueries()
 }
 
 /*
- * handleFileTransferRequest: Function executes on a new thread and handles the UDP, node query messages
+ * handleFileNodeQueries: Function executes on a new thread and handles the UDP, node query messages
  */
-void *handleFileTransferRequest(void *data)
+void *handleFileNodeQueries(void *data)
 {
 	printf("INFO: UDP Thread for handling Node Query messages\n");
+
+	while(1);
+}
+
+/*********************************************************************************************************
+ /** spawnUdpThreadForEntryHandler: Function to spawn a thread to handle UDP messages for JoinReq, JoinResp
+  *
+  *  Block on a UDP Socket for JoinReq message, do the processing and send the JoinResp message
+  *
+  * @return -1 if error
+ *
+ *********************************************************************************************************/
+int spawnUdpThreadForEntryHandler()
+{
+	int status;
+
+	pthread_attr_init(&udpEntryHandlerThreadAttr);
+
+	pthread_attr_setdetachstate(&udpEntryHandlerThreadAttr, PTHREAD_CREATE_JOINABLE);
+
+	if((status = pthread_create(&udpEntryHandlerThread, &udpEntryHandlerThreadAttr, handleNodeEntry, 0)) == -1)
+	{
+		printf("ERROR: spawnUdpThreadForEntryHandler, failed spawning a thread, ECODE %s\n", strerror(errno));
+		return -1;
+	}
+
+	return 0;
+}
+
+/*
+ * handleNodeEntry: Function executes on a new thread and handles the UDP, node query messages
+ */
+void *handleNodeEntry(void *data)
+{
+	printf("INFO: UDP Thread for handling Node Entry\n");
 
 	while(1);
 }
