fa1822473f2d194ecfbecc416fea1c4b820ee9bf
niranjan.quasar <niranjan.quasar@gmail.com>
2013-11-16 00:53:15 +0000

    All message information added. Now need to add handlers to each message.

---  scalable-networks/Common/Constants.h
+++  scalable-networks/Common/Constants.h
@@ -1,9 +1,15 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define MAX_NUMBER_OF_NODES 6
-#define MAX_CHARACTERS_IN_FILENAME 50
-#define MAX_CHARACTERS_IN_HOSTNAME 50
+#define MAX_NUMBER_OF_NODES 					6
+#define MAX_CHARACTERS_IN_FILENAME 				32
+#define MAX_CHARACTERS_IN_HOSTNAME 				32
+#define MAX_CHARACTERS_IN_PORTNUMBER 			8
+#define MAX_NODE_ID_LENGTH 						8
+#define NO_VALUE								0xFFFF
+#define TRUE									1
+#define FALSE									0
+#define MAX_NUM_OF_NON_PRIME_NODES				3
 
 #endif // CONSTANTS_H
 
